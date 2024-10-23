e52c82608d1b6ead8a438911f769c805a5d9090e
Cassiano S. Moreira <cassiano.s.moreira@gmail.com>
2024-10-23 15:41:12 -0700

    Feature: Add mock api function to retrieve chart data from JSON files

---
+++ src/api/charts.ts
@@ -0,0 +1,34 @@
+import { ResponseChart } from './types';
+
+/**
+ * Mocking an API to fetch chart data from multiple JSON endpoints.
+ *
+ * This function makes asynchronous requests to retrieve chart data from four different JSON files.
+ * It throws an error if any of the responses are not OK or if there is a problem during the fetch operation.
+ *
+ * @returns {Promise<ResponseChart[]>} A promise that resolves to an array of chart data responses.
+ *
+ * @throws {Error} Throws an error if the fetch operation fails or if any response is not OK.
+ */
+export const getCharts = async (): Promise<ResponseChart[]> => {
+  try {
+    const res1 = await fetch('/chart1.json');
+    const res2 = await fetch('/chart2.json');
+    const res3 = await fetch('/chart3.json');
+    const res4 = await fetch('/chart4.json');
+
+    // Check if responses are OK
+    if (!res1.ok || !res2.ok || !res3.ok || !res4.ok) {
+      throw new Error('Failed to fetch charts data');
+    }
+
+    const chart1 = await res1.json();
+    const chart2 = await res2.json();
+    const chart3 = await res3.json();
+    const chart4 = await res4.json();
+
+    return [chart1, chart2, chart3, chart4];
+  } catch (error) {
+    throw new Error('Error fetching chart data');
+  }
+};
---
+++ src/api/types.ts
@@ -0,0 +1,50 @@
+export interface ChartDataPoint {
+  x: string;
+  y: number;
+}
+
+export enum ChartType {
+  BAR = 'bar',
+  STACKED_BAR = 'bar - stacked',
+  LINE = 'line',
+  PIE = 'pie',
+}
+
+export interface ResponseBarChart {
+  chartType: ChartType.BAR;
+  chartTitle: string;
+  xAxisLabel: string;
+  yAxisLabel: string;
+  categories?: string[];
+  data: ChartDataPoint[];
+}
+
+export interface ResponseStackedBarChart {
+  chartType: ChartType.STACKED_BAR;
+  chartTitle: string;
+  xAxisLabel: string;
+  yAxisLabel: string;
+  categories?: string[];
+  data: { x: string; values: number[] }[];
+}
+
+export interface ResponseLineChart {
+  chartType: ChartType.LINE;
+  chartTitle: string;
+  xAxisLabel: string;
+  yAxisLabel: string;
+  categories: string[];
+  data: { x: string } & Record<string, number>[];
+}
+
+export interface ResponsePieChart {
+  chartType: ChartType.PIE;
+  chartTitle: string;
+  data: ChartDataPoint[];
+}
+
+export type ResponseChart =
+  | ResponseBarChart
+  | ResponseStackedBarChart
+  | ResponseLineChart
+  | ResponsePieChart;
